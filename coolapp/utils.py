9422428334be2ee2a08288aea1f30cdb739ab34a
Mohammad Rajib <rajibkhan254051@gmail.com>
2023-10-28 04:22:32 +0600

    added get_temperature_forecast_raw function

--- coolapp/utils.py
+++ coolapp/utils.py
@@ -17,13 +17,31 @@ def get_temperature_forecast(lat, long):
         list: A list of floats representing the temperature in degrees Celsius for each hour.
         None: If the API request fails or the data is invalid.
     """
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m&timezone=auto"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m&timezone=auto&forecast_days=7" # forecast_days=(1,16)
     try:
         response = requests.get(url)
         response.raise_for_status() # Raise an exception if the status code is not 200
         data = response.json() # Parse the JSON data
         # Return a list of temperatures using a list comprehension
         return [temp for temp in data["hourly"]["temperature_2m"]]
+    except requests.exceptions.RequestException as e:
+        # Handle any HTTP errors
+        logger.debug(f"An error occurred while requesting {url}: {e}")
+    except json.decoder.JSONDecodeError as e:
+        # Handle any JSON parsing errors
+        logger.debug(f"An error occurred while decoding {response.text}: {e}")
+    return None
+
+
+def get_temperature_forecast_raw(lat, long):
+    """Get the hourly temperature forecast for a given latitude and longitude.
+    """
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m&timezone=auto&forecast_days=7" # forecast_days=(1,16)
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Raise an exception if the status code is not 200
+        data = response.json() # Parse the JSON data
+        return data
     except requests.exceptions.RequestException as e:
         # Handle any HTTP errors
         logger.debug(f"An error occurred while requesting {url}: {e}")
