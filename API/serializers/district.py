561fa3c6f5e913c4d9bc062e0c5761e7621a3233
Mohammad Rajib <rajibkhan254051@gmail.com>
2023-10-27 17:04:18 +0600

    changes in DistrictSerializer

--- API/serializers/district.py
+++ API/serializers/district.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from coolapp.models import District
 
 class DistrictSerializer(serializers.ModelSerializer):
+    # Define an additional field for 'average_temperature_2pm'
+    average_temperature_2pm = serializers.FloatField()
     class Meta:
         model = District
-        fields = '__all__'  # This includes all fields in the serialization
+        fields = [
+            'id', 'name', 'bn_name', 'lat',
+            'long', 'average_temperature_2pm'
+            ]
