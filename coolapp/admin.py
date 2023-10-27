1ee463157b5364212b7b10e6919015ce641a9db4
Mohammad Rajib <rajibkhan254051@gmail.com>
2023-10-27 19:21:27 +0600

    registered TemperatureData model

--- coolapp/admin.py
+++ coolapp/admin.py
@@ -15,3 +15,13 @@ class DistrictModelAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'last_updated')
     list_filter = ['is_active']
 
+
+@admin.register(models.TemperatureData)
+class TemperatureDataModelAdmin(admin.ModelAdmin):
+    list_display = [
+        'district', 'average_temperature_2pm',
+        'created_at', 'last_updated'
+    ]
+    search_fields = ['name', 'bn_name']
+    readonly_fields = ('created_at', 'last_updated')
+    ordering = ('average_temperature_2pm', )
