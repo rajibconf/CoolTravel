9c189b5129a397f63dd9c1f5dadb42a9ed3faf07
Mohammad Rajib <rajibkhan254051@gmail.com>
2023-10-28 18:42:53 +0600

    update linting

--- API/views/cool_district.py
+++ API/views/cool_district.py
@@ -82,11 +82,6 @@ class CoolestDistricts(ListAPIView):
         return coolest_districts
 
 
-# views.py
-
-from django.db.models import Q
-from concurrent.futures import ThreadPoolExecutor
-
 class TravelRecommendation(APIView):
     permission_classes = (permissions.AllowAny,)  # Allow public access
 
@@ -103,14 +98,14 @@ class TravelRecommendation(APIView):
             missing_parameters.append("travel_date")
         if missing_parameters:
             return Response({
-                "error": f"Missing required parameters: {', '.join(missing_parameters)}"
+                "error": f"Missing required parameters: {', '.join(missing_parameters)}" # noqa
             }, status=400)
 
         districts = District.objects.filter(
-            Q(name__iexact=source_location) | Q(name__iexact=destination_location)
+            Q(name__iexact=source_location) | Q(name__iexact=destination_location) # noqa
         ).values('id', 'name', 'bn_name', 'lat', 'long')
-        
-        travel_date_forecast = datetime.strptime(travel_date, "%d-%m-%Y").replace(hour=14, minute=0)
+
+        travel_date_forecast = datetime.strptime(travel_date, "%d-%m-%Y").replace(hour=14, minute=0) # noqa
 
         def fetch_and_process_district(district):
             try:
@@ -119,17 +114,17 @@ class TravelRecommendation(APIView):
                     district_id=district['id'],
                     travel_date=travel_date_forecast
                 )
-                if temperature_data.last_updated < timezone.now() - timezone.timedelta(hours=1):
+                if temperature_data.last_updated < timezone.now() - timezone.timedelta(hours=1): # noqa
                     # Data is outdated, update it
-                    data_dict = get_temperature_forecast_raw(district['lat'], district['long'])
-                    temperature_data.generationtime_ms = data_dict['generationtime_ms']
+                    data_dict = get_temperature_forecast_raw(district['lat'], district['long']) # noqa
+                    temperature_data.generationtime_ms = data_dict['generationtime_ms'] # noqa
                     temperature_data.hourly_time = data_dict["hourly"]["time"]
-                    temperature_data.hourly_temperature_2m = data_dict["hourly"]["temperature_2m"]
+                    temperature_data.hourly_temperature_2m = data_dict["hourly"]["temperature_2m"] # noqa
                     temperature_data.last_updated = timezone.now()
                     temperature_data.save()
             except TemperatureForecast.DoesNotExist:
                 # Data doesn't exist, create a new entry
-                data_dict = get_temperature_forecast_raw(district['lat'], district['long'])
+                data_dict = get_temperature_forecast_raw(district['lat'], district['long']) # noqa
                 forecast = TemperatureForecast(
                     district_id=district['id'],
                     travel_date=travel_date_forecast,
@@ -138,23 +133,23 @@ class TravelRecommendation(APIView):
                     generationtime_ms=data_dict["generationtime_ms"],
                     utc_offset_seconds=data_dict["utc_offset_seconds"],
                     timezone=data_dict["timezone"],
-                    timezone_abbreviation=data_dict["timezone_abbreviation"],
+                    timezone_abbreviation=data_dict["timezone_abbreviation"], # noqa
                     elevation=data_dict["elevation"],
                     hourly_units_time=data_dict["hourly_units"]["time"],
-                    hourly_units_temperature_2m=data_dict["hourly_units"]["temperature_2m"],
+                    hourly_units_temperature_2m=data_dict["hourly_units"]["temperature_2m"], # noqa
                     hourly_time=data_dict["hourly"]["time"],
-                    hourly_temperature_2m=data_dict["hourly"]["temperature_2m"],
+                    hourly_temperature_2m=data_dict["hourly"]["temperature_2m"], # noqa
                     last_updated=timezone.now()
                 )
                 forecast.save()
             return district
 
         with ThreadPoolExecutor() as executor:
-            coolest_districts = list(executor.map(fetch_and_process_district, districts))
+            coolest_districts = list(executor.map(fetch_and_process_district, districts)) # noqa
 
         def get_temperature_at_time(temperature_forecast, target_time):
             target_time = target_time.replace(hour=14, minute=0)
-            for i, forecast_time_str in enumerate(temperature_forecast.hourly_time):
+            for i, forecast_time_str in enumerate(temperature_forecast.hourly_time): # noqa
                 forecast_time = datetime.fromisoformat(forecast_time_str)
                 if forecast_time >= target_time:
                     return temperature_forecast.hourly_temperature_2m[i]
@@ -171,29 +166,28 @@ class TravelRecommendation(APIView):
                 travel_date=travel_date_forecast
             )
 
-            target_time = datetime.strptime(travel_date, "%d-%m-%Y").replace(hour=14, minute=0)
-            source_temperature_2pm = get_temperature_at_time(source_forecast, target_time)
-            destination_temperature_2pm = get_temperature_at_time(destination_forecast, target_time)
+            target_time = datetime.strptime(travel_date, "%d-%m-%Y").replace(hour=14, minute=0) # noqa
+            source_temperature_2pm = get_temperature_at_time(source_forecast, target_time) # noqa
+            destination_temperature_2pm = get_temperature_at_time(destination_forecast, target_time) # noqa
 
             source_location = source_location.title()
             destination_location = destination_location.title()
             recommendation_message = (
-                f"Your {source_location} is {'cooler' if source_temperature_2pm <destination_temperature_2pm else 'warmer'} "
+                f"Your {source_location} is {'cooler' if source_temperature_2pm <destination_temperature_2pm else 'warmer'} " # noqa
                 f"than your destination {destination_location}. "
-                f"{'Stay at home.' if source_temperature_2pm < destination_temperature_2pm else 'Traveling is a good idea.'}"
+                f"{'Stay at home.' if source_temperature_2pm < destination_temperature_2pm else 'Traveling is a good idea.'}" # noqa
             )
 
             response_data = {
                 "source_location": source_location,
                 "destination_location": destination_location,
                 "source_temperature_2pm": f"{source_temperature_2pm}°C",
-                "destination_temperature_2pm": f"{destination_temperature_2pm}°C",
+                "destination_temperature_2pm": f"{destination_temperature_2pm}°C", # noqa
                 "recommendation_message": recommendation_message
             }
 
             return Response(response_data)
         except TemperatureForecast.DoesNotExist:
             return Response({
-                "error": "Data not found for the given location and travel date"
+                "error": "Data not found for the given location and travel date" # noqa
             }, status=400)
-
